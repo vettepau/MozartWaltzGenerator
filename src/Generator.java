a358d0343d79412beed3f95db971bc55bc8bcb53
jewbank <jewbank@conroeisd.net>
2020-03-30 13:23:40 -0500

    Class 3/30/2020

--- src/Generator.java
+++ src/Generator.java
@@ -2,9 +2,11 @@ public class Generator {
     public static void main(String[] args) {
 
         // An example of how to play M1
-        double[] sample1 = StdAudio.read("mozart/M1.wav");
-        StdAudio.play(sample1);
 
+        for(int i = 1; i <= 172; i++) {
+            double[] sample = StdAudio.read("mozart/M"+i+".wav");
+            StdAudio.play(sample);
+        }
 
     }
 }

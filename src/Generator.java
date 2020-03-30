4e21a71de80ecf0b808925f181436596fad6d096
jewbank <jewbank@conroeisd.net>
2020-03-30 13:02:02 -0500

    Initial Commit

---
+++ src/Generator.java
@@ -0,0 +1,10 @@
+public class Generator {
+    public static void main(String[] args) {
+
+        // An example of how to play M1
+        double[] sample1 = StdAudio.read("mozart/M1.wav");
+        StdAudio.play(sample1);
+
+
+    }
+}
